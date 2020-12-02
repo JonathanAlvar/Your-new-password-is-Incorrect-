e2ddb73374609a431cfaeb275dadfd278e50f19d
Jonathan <homerj65@gmail.com>
2020-12-01 17:54:04 -0700

    Moved for loop into function and updated for loop to push user password to web page

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.querySelector("#password");
 //My arrays
+var _specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
+var _numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var _lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var _upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//var userChoice = [];
+//var password = generatePassword();Where to use?
 
-var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var userChoice = [];
 
-//Generating a password
+//User choice to generate a password
 function writePassword() {
-  var passwordText = document.querySelector("#password");
+
 
   var passwordLength = prompt("How long would you like your password? Must be between 8 - 128 characters")
   if (passwordLength <= 128 && passwordLength >= 8) {
@@ -56,32 +57,35 @@ function writePassword() {
   else {
     alert("Please enter a password length between 8 and 128.");
   }
-};
 
+  console.log(numbers)
 
-//Generating user password from array. Using concat method to join.
+  //Generating user password from array. Using concat method to join.
+  var userChoice = [];
+  if (specialCharacters) {
+    userChoice = userChoice.concat(_specialCharacters);
+  }
+  if (numbers) {
+    userChoice = userChoice.concat(_numbers);
+  }
+  if (lowerCase) {
+    userChoice = userChoice.concat(_lowerCase);
+  }
+  if (upperCase) {
+    userChoice = userChoice.concat(_upperCase);
+  }
+  console.log(userChoice)
 
-var userChoice = [];
-if (specialCharacters) {
-  userChoice = userChoice.concat(specialCharacters);
+  //Looping the arrays to send user password.
+  var userPassword = [];
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomPasswordIndex = Math.floor(Math.random() * userChoice.length);
+    var randomChar = userChoice[randomPasswordIndex]
+    userPassword.push(randomChar);
+  }
+  console.log(userPassword.join(""));
+  passwordText.value = userPassword.join("");
 }
-if (numbers) {
-  userChoice = userChoice.concat(numbers);
-}
-if (lowerCase) {
-  userChoice = userChoice.concat(lowerCase);
-}
-if (upperCase) {
-  userChoice = userChoice.concat(upperCase);
-}
-
-//Looping the arrays
-var userPassword = [i];
-for (var i = 0; i <= length; i++) {
-  var randomPassword = Math.floor(Math.random() * userChoice.length);
-  userPassword.push(randomPassword);
-}
-console.log(userPassword.push(randomPassword));
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+console.log(generateBtn);
